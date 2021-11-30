66a621cba4ee20cecd51bc71f982590124d4e9b2
Chris Reynolds <chris@jazzsequence.com>
2021-11-30 10:57:49 -0700

    add all the things that would be served in separate packages

---
+++ packages/pantheon-wp-muplugin/pantheon.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Pantheon
+ * Plugin URI: https://pantheon.io/
+ * Description: Building on Pantheon's and WordPress's strengths, together.
+ * Version: 0.1
+ * Author: Pantheon
+ * Author URI: https://pantheon.io/
+ *
+ * @package pantheon
+ */
+
+if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
+
+	require_once 'inc/pantheon-page-cache.php';
+	if ( ! defined( 'DISABLE_PANTHEON_UPDATE_NOTICES' ) || ! DISABLE_PANTHEON_UPDATE_NOTICES ) {
+		require_once 'pantheon/pantheon-updates.php';
+	}
+	if ( ! defined('RETURN_TO_PANTHEON_BUTTON') || RETURN_TO_PANTHEON_BUTTON ) {
+		require_once 'inc/pantheon-login-form-mods.php';
+	}
+} // Ensuring that this is on Pantheon.
